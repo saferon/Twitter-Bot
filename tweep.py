dc67461b4f4c2727b397edd7f9500333ecf2014c
Saferon <cameronshandlite@gmail.com>
2020-05-05 21:39:46 +0100

    changed file name, fixed errors

---
+++ tweep.py
@@ -0,0 +1,61 @@
+import tweepy
+import time
+import random
+import os
+import threading
+import datetime
+import json
+with open('wjsn_paths.json', 'r') as used_paths:
+    used_paths_list = json.load(used_paths)
+        
+with open('keys.json', 'r') as keys:
+    keys_dict = json.load(keys)
+
+auth = tweepy.OAuthHandler(keys_dict["api"], keys_dict["secret"])
+
+auth.set_access_token(keys_dict["auth"], keys_dict["authsec"])
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+try:
+    api.verify_credentials()
+    print("Tweet bot online.")
+except:
+    print("Error during authentication")
+
+user = api.me()
+
+path_to_images = '/media/pi/8A02-DF82/wjsn/'
+
+def tweet_random_image():
+    """tweet random picture with the name of who is in the picture"""
+    threading.Timer(3600.0, tweet_random_image).start()  # sets timing for tweets
+    with open('wjsn_paths.json', 'r') as used_paths:
+        used_paths_list = json.load(used_paths)
+    paths = []
+    # list out all of the directories of the path
+    for directory in os.listdir(path_to_images):
+        paths.append(directory)  # append these directories to the list paths
+
+    alpha = random.choice(paths)
+    beta = os.listdir(path_to_images + alpha + '/')
+    beta_refined = []
+    for images in beta:
+        if ".webm" not in images and ".mp4" not in images and ".gif" not in images and ".ini" not in images:
+            if path_to_images + alpha + images not in used_paths_list["list"]:
+                beta_refined.append(images)
+                used_paths_list["list"].append(path_to_images + alpha + images)  # add full file name for duplicate file names in different directories
+                with open('wjsn_paths', 'w') as pathdump:
+                    json.dump(used_paths_list, pathdump)
+
+    gamma = random.choice(beta_refined)
+    delta = path_to_images + alpha + '/' + gamma
+    if alpha == "wjsn":
+        api.update_with_media(delta, '#WJSN #우주소녀')
+    else:
+        api.update_with_media(delta, '#WJSN #우주소녀 #' + alpha.title())
+    print("tweeted " + delta + " at " + str(datetime.datetime.now()))
+
+
+tweet_random_image()
+
