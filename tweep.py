fe9823c94a8331078d2bff4b98e8bb15b49b474f
Saferon <cameronshandlite@gmail.com>
2020-05-12 02:17:51 +0100

    include followback

--- tweep.py
+++ tweep.py
@@ -5,14 +5,11 @@ import os
 import threading
 import datetime
 import json
-with open('wjsn_paths.json', 'r') as used_paths:
-    used_paths_list = json.load(used_paths)
-        
+
 with open('keys.json', 'r') as keys:
     keys_dict = json.load(keys)
 
 auth = tweepy.OAuthHandler(keys_dict["api"], keys_dict["secret"])
-
 auth.set_access_token(keys_dict["auth"], keys_dict["authsec"])
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
@@ -27,11 +24,19 @@ user = api.me()
 
 path_to_images = '/media/pi/8A02-DF82/wjsn/'
 
+def follow_back():
+    threading.Timer(300.0, follow_back).start()  # sets timing for checking for followers
+    for follower in tweepy.Cursor(api.followers).items():
+        try:
+            follower.follow()
+            print("followed:" + str(follower.screen_name))
+        except:
+            pass
+
+
 def tweet_random_image():
     """tweet random picture with the name of who is in the picture"""
     threading.Timer(3600.0, tweet_random_image).start()  # sets timing for tweets
-    with open('wjsn_paths.json', 'r') as used_paths:
-        used_paths_list = json.load(used_paths)
     paths = []
     # list out all of the directories of the path
     for directory in os.listdir(path_to_images):
@@ -41,10 +46,12 @@ def tweet_random_image():
     beta = os.listdir(path_to_images + alpha + '/')
     beta_refined = []
     for images in beta:
+        with open('wjsn_paths.json', 'r') as used_paths:
+            used_paths_list = json.load(used_paths)
         if ".webm" not in images and ".mp4" not in images and ".gif" not in images and ".ini" not in images:
-            if path_to_images + alpha + images not in used_paths_list["list"]:
+            if path_to_images + alpha + images not in used_paths_dict["list"]:
                 beta_refined.append(images)
-                used_paths_list["list"].append(path_to_images + alpha + images)  # add full file name for duplicate file names in different directories
+                used_paths_list.append(path_to_images + alpha + images)  # add full file name for duplicate file names in different directories
                 with open('wjsn_paths', 'w') as pathdump:
                     json.dump(used_paths_list, pathdump)
 
@@ -56,6 +63,6 @@ def tweet_random_image():
         api.update_with_media(delta, '#WJSN #우주소녀 #' + alpha.title())
     print("tweeted " + delta + " at " + str(datetime.datetime.now()))
 
-
 tweet_random_image()
+follow_back()
 
